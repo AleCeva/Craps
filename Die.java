f8854fbe904129d7b65ca518e917d55a4cc00fc4
BulletCat01 <81199016+BulletCat01@users.noreply.github.com>
2022-01-14 09:34:10 -0800

    Completed a draft of the Die.java file

--- Die.java
+++ Die.java
@@ -1,3 +1,22 @@
-public class Die{
+public class Die
+{
     
+    private int roll;
+
+   
+    public Die()
+    {
+       
+        roll = 1;
+    }
+
+
+    public int rollDice()
+    {
+       
+        int roll1 = (int) (Math.random() * 6) + 1;
+        int roll2 = (int) (Math.random() * 6) + 1;
+        int total = roll1 + roll2;
+        return total;
+    }
 }
