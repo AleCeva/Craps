be4dae0bf916fc6a19ff2ffb15ad5215b89baeaa
BulletCat01 <81199016+BulletCat01@users.noreply.github.com>
2022-01-19 11:04:09 -0800

    Added comments to Die.java file

--- Die.java
+++ Die.java
@@ -1,22 +1,24 @@
 public class Die
 {
-    
+  //Declare Variables  
     private int roll;
 
-   
+  //Constructor 
     public Die()
     {
        
         roll = 1;
     }
 
-
+    //Method
     public int rollDice()
     {
-       
+       //Creats two dice rolls and ads them to produce the craps total for that round
         int roll1 = (int) (Math.random() * 6) + 1;
         int roll2 = (int) (Math.random() * 6) + 1;
         int total = roll1 + roll2;
+       
         return total;
+
     }
 }
